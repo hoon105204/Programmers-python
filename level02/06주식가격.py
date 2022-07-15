278d974081ee1493f9d27562158c9c1250295d70
hoon105204 <hoon105204@gmail.com>
2022-07-15 23:55:12 +0900

    Changes

--- level02/06주식가격.py
+++ level02/06주식가격.py
@@ -4,9 +4,9 @@ def solution(prices):
     answer = [0]*len(prices)
 
     for i in range(len(prices)-1):
-        for j in range(i+1,len(prices)):
+        for j in range(i+1, len(prices)):
             answer[i] += 1
-            if prices[i]>prices[j]:
+            if prices[i] > prices[j]:
                 break
     return answer
 
