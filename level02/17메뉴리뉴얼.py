b3bbf668b000b6ebae973f1ddb92c3e4df74696f
user <hoon105204@gmail.com>
2023-02-10 17:52:47 +0900

    update

---
+++ level02/17메뉴리뉴얼.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/72411
+def solution(orders, course):
+    answer = []
+    return answer
+
+
+orders, course = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]
+print(solution(orders, course))
